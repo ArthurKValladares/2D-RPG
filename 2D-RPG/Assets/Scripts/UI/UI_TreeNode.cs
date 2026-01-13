a068d854b1711c42f6e8aab51db8ecca76d36f54
ArthurKValladares <arthurkaukal@gmail.com>
2026-01-12 21:55:00 -0300

    starting on UI

---
+++ 2D-RPG/Assets/Scripts/UI/UI_TreeNode.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_TreeNode : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    [SerializeField] private Image skillIcon;
+
+    [SerializeField] private string skillLockedColorHex = "#6E6E6E";
+    private Color skillLockedColor;
+    [SerializeField]  private float highlightIntensity = 1.5f;
+
+    private bool isLearned;
+    private bool isLocked;
+
+    private void Awake()
+    {
+        skillLockedColor = GetColorByHex(skillLockedColorHex);
+
+        SetColor(skillLockedColor);    
+    }
+
+    private void Learn()
+    {
+        isLearned = true;
+        SetColor(Color.white);
+    }
+
+    private bool CanBeLearned()
+    {
+        if (isLocked || isLearned) return false;
+        return true;
+    }
+
+    private void SetColor(Color color)
+    {
+        if (!skillIcon) return;
+
+        skillIcon.color = color;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (CanBeLearned())
+        {
+            Learn();
+        }
+        else
+        {
+            Debug.Log("cannot be learned");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isLearned)
+        {
+            SetColor(skillLockedColor * highlightIntensity);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isLearned)
+        {
+            SetColor(skillLockedColor);
+        }
+    }
+
+    private Color GetColorByHex(string hex)
+    {
+        Color color;
+        ColorUtility.TryParseHtmlString(skillLockedColorHex, out color);
+        return color;
+    }
+}
