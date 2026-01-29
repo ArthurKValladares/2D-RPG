93b4a85b34b8fd8efae2bf7ec27c4e84df8e7708
ArthurKValladares <arthurkaukal@gmail.com>
2026-01-29 18:41:02 -0300

    some domain stuff

---
+++ 2D-RPG/Assets/Scripts/SkillSystem/SkillObjects/SkillObject_DomainExpansion.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class SkillObject_DomainExpansion : SkillObject_Base
+{
+    private Skill_DomainExpansion domainExpansion;
+
+    private float maximumSizeScale;
+    private float transformationDuration;
+    private float domainDuration;
+    private float slowDownPercentage;
+
+    private float targetScale;
+    private float startTransformationTime;
+
+    private bool isShrinking;
+
+    public void SetupDomain(Skill_DomainExpansion domainExpansion)
+    {
+        this.domainExpansion = domainExpansion;
+
+        this.maximumSizeScale = domainExpansion.maximumSizeScale;
+        this.transformationDuration = domainExpansion.transformationDuration;
+        this.domainDuration = domainExpansion.GetDomainDuration();
+        this.slowDownPercentage = domainExpansion.GetSlowDownPercentage();
+
+        targetScale = maximumSizeScale;
+        startTransformationTime = Time.time;
+
+        Invoke(nameof(ShrinkDomain), domainDuration);
+    }
+
+    private void Update()
+    {
+        HandleScaling();
+    }
+
+    private void HandleScaling()
+    {
+        float elapsedTime = Time.time - startTransformationTime;
+
+        if (elapsedTime < transformationDuration)
+        {
+            Vector3 finalScaleVector = new Vector3(targetScale, targetScale, targetScale);
+            transform.localScale = Vector3.Lerp(transform.localScale, finalScaleVector, elapsedTime / transformationDuration);
+        }
+
+        if (elapsedTime >= transformationDuration && isShrinking)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShrinkDomain()
+    {
+        targetScale = 0.0f;
+        startTransformationTime = Time.time;
+        
+        isShrinking = true;
+    }
+}
--- 2D-RPG/Assets/Scripts/SkillSystem/Skills/Skill_DomainExpansion.cs
+++ 2D-RPG/Assets/Scripts/SkillSystem/Skills/Skill_DomainExpansion.cs
@@ -1,7 +1,26 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Skill_DomainExpansion : Skill_Base
 {
+    [SerializeField] private GameObject domainExpansionPrefab;
+
+    [Header("Core Details")]
+    [SerializeField] public float maximumSizeScale = 10.0f;
+    [SerializeField] public float transformationDuration = 0.2f;
+
+    [Header("Slow Down/Base Upgrade")]
+    [SerializeField] private float baseDomainDuration = 5.0f;
+    [SerializeField] private float baseSlowDownPercentage = 0.9f;
+
+    [Header("Shard Upgrade")]
+    [SerializeField] private float shardDomainDuration = 5.0f;
+    [SerializeField] private float shardSlowDownPercentage = 0.9f;
+
+    [Header("Echo Upgrade")]
+    [SerializeField] private float echoDomainDuration = 5.0f;
+    [SerializeField] private float echoSlowDownPercentage = 0.9f;
+
     public bool InstantDomain()
     {
         return !IsLearned(SkillUpgradeType.Domain_EchoSpam) && !IsLearned(SkillUpgradeType.Domain_ShardSpam);
@@ -9,6 +28,42 @@ public class Skill_DomainExpansion : Skill_Base
 
     public void CreateDomain()
     {
-        Debug.Log("Create Skill Object");
+        GameObject domain = Instantiate(domainExpansionPrefab, transform.position, Quaternion.identity);
+        SkillObject_DomainExpansion domainObject = domain.GetComponent<SkillObject_DomainExpansion>();
+        domainObject.SetupDomain(this);
+    }
+
+    public float GetDomainDuration()
+    {
+        if (IsLearned(SkillUpgradeType.Domain_SlowDown))
+        {
+            return baseDomainDuration;
+        } else if (IsLearned(SkillUpgradeType.Domain_ShardSpam))
+        {
+            return shardDomainDuration;
+        } else if (IsLearned(SkillUpgradeType.Domain_EchoSpam))
+        {
+            return echoDomainDuration;
+        }
+        Debug.LogError("Did not implement GetDuration for upgrade of type: " + upgradeType);
+        return baseDomainDuration;
+    }
+
+    public float GetSlowDownPercentage()
+    {
+        if (IsLearned(SkillUpgradeType.Domain_SlowDown))
+        {
+            return baseSlowDownPercentage;
+        }
+        else if (IsLearned(SkillUpgradeType.Domain_ShardSpam))
+        {
+            return shardSlowDownPercentage;
+        }
+        else if (IsLearned(SkillUpgradeType.Domain_EchoSpam))
+        {
+            return echoSlowDownPercentage;
+        }
+        Debug.LogError("Did not implement GetSlowDownPercentage for upgrade of type: " + upgradeType);
+        return baseSlowDownPercentage;
     }
 }
